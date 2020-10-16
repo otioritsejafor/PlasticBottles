2895753940e768769aa96c1ebe1eb5ab4c6e1d6a
Gh0st <otioritsejafor@gmail.com>
2020-10-16 14:24:00 -0500

    Added networking API for fetching network data

--- Sources/PlasticBottles/Networking.swift
+++ Sources/PlasticBottles/Networking.swift
@@ -10,5 +10,28 @@ import Foundation
 extension PlasticBottles {
     public class Networking {
         
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler:
+                                    @escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+            
+        }
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
+        
     }
 }
--- Tests/PlasticBottlesTests/PBNetworkingTests.swift
+++ Tests/PlasticBottlesTests/PBNetworkingTests.swift
@@ -6,16 +6,30 @@
 //
 
 import XCTest
+@testable import PlasticBottles
 
 final class PBNetworkingTests: XCTestCase {
     
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoadDataCall() {
+        let manager = PlasticBottles.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://github.com/otioritsejafor") else {
+            return XCTFail("Could not create URL properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
     }
     
     static var allTests = [
-        ("testExample", testExample)
+        ("testLoadDataCall", testLoadDataCall)
     ]
 
 }
