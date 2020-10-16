0a56c511a70501dccb5cea01b9a0d428d31145b5
Gh0st <otioritsejafor@gmail.com>
2020-10-16 15:16:03 -0500

    Refactored first Networking API to include Dependency injection for easier testing

--- Sources/PlasticBottles/Networking.swift
+++ Sources/PlasticBottles/Networking.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { (data, _, error) in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
+    
+}
+
 extension PlasticBottles {
     public class Networking {
         
@@ -15,15 +30,19 @@ extension PlasticBottles {
         public class Manager {
             public init() {}
             
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
+            
+            /// Fetches data from the live internet
+            /// - Parameters:
+            ///   - url: The location you wish to fetch data from
+            ///   - completionHandler: Returns a result object which signifies the status of the request
             public func loadData(from url: URL, completionHandler:
                                     @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from:url ) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
                 }
-                task.resume()
             }
             
         }
--- Tests/PlasticBottlesTests/PBNetworkingTests.swift
+++ Tests/PlasticBottlesTests/PBNetworkingTests.swift
@@ -8,19 +8,33 @@
 import XCTest
 @testable import PlasticBottles
 
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+    
+    
+}
+
 final class PBNetworkingTests: XCTestCase {
     
     func testLoadDataCall() {
         let manager = PlasticBottles.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
         let expectation = XCTestExpectation(description: "Called for data")
-        guard let url = URL(string: "https://github.com/otioritsejafor") else {
-            return XCTFail("Could not create URL properly")
-        }
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
+        
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnedData):
-                XCTAssertNotNil(returnedData, "Response data is nil")
+                XCTAssertEqual(data, returnedData, "manager returned unexpected data")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "Error forming error result")
             }
