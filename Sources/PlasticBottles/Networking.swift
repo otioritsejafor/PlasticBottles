aa5fcd6ef542ed8ea7b70ae8515b0416838c3684
Gh0st <otioritsejafor@gmail.com>
2020-10-18 20:26:06 -0500

    Added second Networking API for sending data

--- Sources/PlasticBottles/Networking.swift
+++ Sources/PlasticBottles/Networking.swift
@@ -9,16 +9,23 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    func post(with request: URLRequest, completionHandler: @escaping(Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkSession {
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { (data, _, error) in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         let task = dataTask(with: url) { (data, _, error) in
             completionHandler(data, error)
         }
         task.resume()
     }
-    
     
 }
 
@@ -33,7 +40,7 @@ extension PlasticBottles {
             internal var session: NetworkSession = URLSession.shared
             
             
-            /// Fetches data from the live internet
+            /// Fetches data from a URL
             /// - Parameters:
             ///   - url: The location you wish to fetch data from
             ///   - completionHandler: Returns a result object which signifies the status of the request
@@ -45,7 +52,34 @@ extension PlasticBottles {
                 }
             }
             
+            
+            /// Sends data to a URL
+            /// - Warning: Make sure that the URL in question can accept a POST route
+            /// - Parameters:
+            ///   - url: The location to send data to
+            ///   - body: The object to send over the network
+            ///   - completionHandler: Returns a result object which signifies the status of the request
+            public func sendData<RequestType: Codable>(to url: URL, body: RequestType,
+                                                       completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { (data, error) in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch let error {
+                    return completionHandler(.failure(error))
+                }
+                
+            }
+            
+            
         }
+        
+        
         
         public enum NetworkResult<Value> {
             case success(Value)
--- Tests/PlasticBottlesTests/PBNetworkingTests.swift
+++ Tests/PlasticBottlesTests/PBNetworkingTests.swift
@@ -16,7 +16,14 @@ class NetworkSessionMock: NetworkSession {
         completionHandler(data, error)
     }
     
-    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class PBNetworkingTests: XCTestCase {
@@ -42,8 +49,35 @@ final class PBNetworkingTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
     
+    func testSendDataCall() {
+        let manager = Bottles.Networking.Manager()
+        let session = NetworkSessionMock()
+        let testObject = MockData(id: 1, name: "Oti")
+        let data = try? JSONEncoder().encode(testObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expecation = XCTestExpectation(description: "Sent data")
+        
+        manager.sendData(to: url, body: testObject) { result in
+            expecation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, testObject)
+                break
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming result")
+            }
+            
+        }
+        wait(for: [expecation], timeout: 5)
+        
+    }
+    
     static var allTests = [
-        ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall)
     ]
 
 }
